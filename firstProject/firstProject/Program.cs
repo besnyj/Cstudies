ad19ad66a23f61ea141c60b63a60307b2f80ef68
Besnyj <verificacoesfelipe@gmail.com>
2024-03-11 14:31:41 -0300

    studying

--- firstProject/firstProject/Program.cs
+++ firstProject/firstProject/Program.cs
@@ -1,10 +1,45 @@
 ﻿using System;
+using System.CodeDom;
 
-string word;
 
-do
-{
-    Console.WriteLine("enter a word longer than 10 letters");
-    word = Console.ReadLine();
-}
-while(word.Length <= 10);
+// int result = 0;
+// for (int i = 0; i < numbers.Length; i++)
+// {
+//     result += numbers[i];
+// }
+// Console.WriteLine(result);
+//
+// char[,] letters = new char[2, 3];
+//
+// letters[0, 0] = 'A';
+//
+// var letters2 = new char[,]
+// {
+//     {'A', 'B', 'C'},
+//     {'D', 'E', 'F'}
+// };
+//
+// // var x = letters2.GetLength(0);
+// // var y = letters2.GetLength(1);
+// //
+// // for (int i = 0; i < x; i++)
+// // {
+// //     for (int j = 0; j < y; j++)
+// //     {
+// //         Console.WriteLine(letters2[i, j]);
+// //     }
+// // }
+
+// int[] numbers = new int[] { 2, 6, 1, 6, 19};
+//
+// for (int i = 0; i < numbers.Length; i++)
+// {
+//     Console.WriteLine(numbers[i]);
+// }
+//
+// int result = 0;
+// foreach (var number in numbers)
+// {
+//     result += number;
+// }
+// Console.WriteLine(result);
