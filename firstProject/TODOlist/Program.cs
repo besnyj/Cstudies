07da5c125f845ae31e5cb12d257d33043d1c2348
Besnyj <verificacoesfelipe@gmail.com>
2024-03-14 10:07:02 -0300

    finished first section of the course

--- firstProject/TODOlist/Program.cs
+++ firstProject/TODOlist/Program.cs
@@ -179,3 +179,137 @@ bool ValidUserChoice(string choice)
         default: return false;
     }
 }
+
+// -=-=-=-=- PROFESSOR VERSION -=-=-=-=-=--=
+// whenever creating switches, prefer to use methods for each condition instead of writing it in the switch brackets instead
+// remember that the following code is refactored. It was not first wrote like that. https://www.udemy.com/course/ultimate-csharp-masterclass/learn/lecture/33511688#overview
+Console.WriteLine("What do you want to do? ");
+
+bool shallExit = false;
+while (!shallExit)
+{
+
+
+    Console.WriteLine("[S]ee all todos ");
+    Console.WriteLine("[A]dd a todo ");
+    Console.WriteLine("[R]emove a todo ");
+    Console.WriteLine("[E]xit");
+
+    var userChoice = Console.ReadLine();
+    switch (userChoice)
+    {
+        case "E":
+        case "e":
+            shallExit = true;
+            break;
+        case "S":
+        case "s":
+            SeeAllTodos();
+            break;
+        case "A":
+        case "a":
+            AddTodo();
+            break;
+        case "R":
+        case "r":
+            RemoveTodo();
+            break;
+        default:
+            Console.WriteLine("Invalid choice");
+            break;
+    }
+}
+
+void AddTodo()
+{
+    string description;
+    do
+    {
+        Console.WriteLine("Enter the TODO description");
+        description = Console.ReadLine();
+
+    } while (!IsDescriptionValid(description));
+
+    todos.Add(description);
+}
+
+void SeeAllTodos()
+{
+    if (todos.Count == 0)
+    {
+        NoTODOsAdded();
+        return;
+    }
+    for (int i = 0; i < todos.Count; i++)
+    {
+        Console.WriteLine($"{i+1}. {todos[i]}");
+    }
+}
+
+void RemoveTodo()
+{
+    if (todos.Count == 0)
+    {
+        NoTODOsAdded();
+        return;
+    }
+
+    int index;
+    do
+    {
+        Console.WriteLine("Select the index of the TODO you want to remove");
+        SeeAllTodos();
+    } while (!TryReadIndex(out index));
+
+    RemoveTodoAtIndex(index);
+}
+
+bool TryReadIndex(out int index)
+{
+    var userInput = Console.ReadLine();
+    if (userInput == "")
+    {
+        index = 0;
+        Console.WriteLine("Selected index cannot be empty");
+        return false;
+    }
+
+    if (int.TryParse(userInput, out index) &&
+        index >= 1 &&
+        index <= todos.Count)
+    {
+        return true;
+    }
+    Console.WriteLine("The given index is not valid.");
+    return false;
+}
+
+
+void NoTODOsAdded()
+{
+    Console.WriteLine("Not TODOs have been added");
+}
+
+void RemoveTodoAtIndex(int index)
+{
+    var todoToBeRemoved = todos[index];
+    todos.RemoveAt(index);
+    Console.WriteLine("TODO removed: " + todoToBeRemoved);
+}
+
+bool IsDescriptionValid(string description)
+{
+    if (description == "")
+    {
+        Console.WriteLine("The description cannot be empty");
+        return false;
+    }
+
+    if (todos.Contains(description))
+    {
+        Console.WriteLine("The description must be unique");
+        return false;
+    }
+
+    return true;
+}
