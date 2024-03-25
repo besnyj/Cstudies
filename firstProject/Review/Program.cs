185573b09e202d077212d512dc0c2d87502ed7a3
Besnyj <verificacoesfelipe@gmail.com>
2024-03-25 16:35:57 -0300

    studying

--- firstProject/Review/Program.cs
+++ firstProject/Review/Program.cs
@@ -1,1 +1,15 @@
 ﻿
+public class Dog
+{
+    private string name;
+    private string breed;
+    private int weight;
+
+    public Dog(string name, string breed, int weight)
+    {
+        name = name;
+        breed = breed;
+        weight = weight;
+    }
+
+}
--- firstProject/firstProject/Program.cs
+++ firstProject/firstProject/Program.cs
@@ -1,1 +1,31 @@
 ﻿
+using System;
+
+Dog cachorro1 = new Dog("Max", 30);
+Dog cachorro2 = new Dog("Pog", breed: "Max", weight: 30);
+
+cachorro1.Describe();
+cachorro2.Describe();
+
+
+public class Dog
+{
+    private string _name;
+    private string _breed;
+    public int Weight;
+
+    public Dog(string name, string breed, int weight)
+    {
+        _name = name;
+        _breed = breed;
+        Weight = weight;
+    }
+
+    public Dog(string name, int weight) : this(name, "mixed-breed", weight)
+    {
+    }
+
+    public void Describe() =>
+        Console.WriteLine($"Name: {this._name}, Breed: {this._breed}, Weight: {this.Weight}");
+
+}
