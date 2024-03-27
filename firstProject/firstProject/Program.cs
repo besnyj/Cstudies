3ade7ee480ccb10e2e9252834245e40cf03d45c3
Besnyj <verificacoesfelipe@gmail.com>
2024-03-27 19:09:49 -0300

    studying

--- firstProject/firstProject/Program.cs
+++ firstProject/firstProject/Program.cs
@@ -1,20 +1,21 @@
 ﻿using System;
 
 Square square1 = new Square(10, 10);
-Console.WriteLine(square1.Height);
-square1.NumberOfSides = 10;
+
+var calculation = Square.CalculateArea(10, 10);
+Console.WriteLine(calculation);
 
 
 class Square
 {
-    public readonly int Height;
-    private readonly int Width;
-    public readonly int NumberOfSides;
+    private int _width;
+    public int Height { get; }
 
     public Square(int height, int width)
     {
         Height = height;
-        Width = width;
-        NumberOfSides = 4;
+        _width = width;
     }
+
+    public static int CalculateArea(int x, int y) => x * y;
 }
