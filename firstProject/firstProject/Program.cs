eb1a899e852ab23197dd389d1a53fc8009df8486
Besnyj <verificacoesfelipe@gmail.com>
2024-04-05 15:35:22 -0300

    studying

--- firstProject/firstProject/Program.cs
+++ firstProject/firstProject/Program.cs
@@ -1,1 +1,30 @@
-﻿
+﻿using System;
+
+Food tomato = new Tomato("Vegetable", 10);
+public class Food
+{
+    public virtual string Name => "This is a food";
+    public virtual int Quantity => 0;
+    public string Type { get; }
+
+    public Food(string type)
+    {
+        this.Type = type;
+    }
+
+}
+
+public class Tomato : Food
+{
+
+    public override int Quantity { get; }
+
+    public Tomato(string type, int quantity) : base(type)
+    {
+        Quantity = quantity;
+    }
+
+    public override string Name => $"Tomato {base.Name}";
+    public override string ToString() => Name;
+}
+
